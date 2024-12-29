ab0f137140632c36464f8bc60ffd74a3da92388b
devCode21 <gwkadak@gmail.com>
2024-12-29 19:02:42 +0530

    Update Project.jsx

--- Proto/src/components/Project.jsx
+++ Proto/src/components/Project.jsx
@@ -9,7 +9,7 @@ function Project({ProjectName,Content, Image, id}) {
             <img src={Image} alt="" />
         </div>
         <div className="veiwproject">
-            <button> <a href="https://web-server-brown.vercel.app/" style={{textDecoration:'none',color:'black'}}>view Project</a></button>
+            <button> <a href="https://web-server-git-main-devcode21s-projects.vercel.app/" style={{textDecoration:'none',color:'black'}}>view Project</a></button>
             <button > <a href="https://github.com/devCode21/webServer"  style={{textDecoration:'none',color:'black'}}>Source Code</a></button>
         </div>
         <div className="ProContent">
